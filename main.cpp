3c3e4deccac6874f8e818aec02d150ecf1a4b614
Tomas Krejci <tomas789@gmail.com>
2013-10-24 21:20:30 +0200

    basic genetic functionality

--- main.cpp
+++ main.cpp
@@ -2,44 +2,21 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <random>
+#include <set>
+#include <functional>
+#include <iomanip>
+#include <mutex>
+#include <thread>
 
-#include "expression.hpp"
+#include "genetics.hpp"
 
-std::map<std::string, double> val_cache;
 
-double eval(const std::string & var_name);
 
 int main(int argc, char ** argv) {
-    (std::cout << "Type expression : ").flush();
-    std::string r;
-    std::getline(std::cin, r);
-
-    try {
-        expression e = parse(r);
-        auto c = e.evaluate(eval);
-
-        std::cout << "INPUT : " << e << std::endl
-                  << "DERIVED BY x : " << e.derive("x") << std::endl
-                  << "EVALUATED : " << c << std::endl
-                  << "EVALUATED DERIVATION BY x : " << e.derive("x").evaluate(eval) << std::endl
-                  << "IS CONSTANT : " << e.is_constant() << std::endl;
-
-    } catch (std::string e) {
-        std::cerr << "EXCEPTION: " << e << std::endl;
-    }
+    genetics g;
+    g.island();
 
     return 0;
 }
 
-double eval(const std::string & var_name) {
-    auto it = val_cache.find(var_name);
-    if (it != val_cache.end()) return it->second;
-    
-    std::cout << " -=- ENTER VALUE FOR " << var_name << " : ";
-    std::cout.flush();
-
-    double c;
-    std::cin >> c;
-    
-    return val_cache[var_name] = c;
-}
